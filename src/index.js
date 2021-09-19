9d0cf90889cb75d9fd378a95666efaa02cf2fe00
Md Faisal Hossain <faisalh.iu@gmail.com>
2021-09-19 23:32:26 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import reactDom from "react-dom";
+import App from "./components/App"
+
+
+
+
+reactDom.render(<App/>, document.querySelector('#root'))
+
+//AIzaSyDXkZDFbCaLIGYacN8_nClRv8HTsesQ_kU
