aef3bf80902e179f1b7244be843411648a71edc2
Md Faisal Hossain <faisalh.iu@gmail.com>
2021-09-20 01:24:20 +0200

    css updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import SearchBar from './SearchBar'
 import youtube from "../api/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 
 class App extends React.Component{
  state = {videos: [], selectedVideo: null }
+
+ componentDidMount (){
+   this.onTermSubmit('sea')
+ }
 
  onTermSubmit = async term => {
   const res = await youtube.get('/search',{
@@ -14,20 +19,34 @@ class App extends React.Component{
      }
    })
 
-   this.setState({videos: res.data.items})
+   this.setState({
+     videos: res.data.items,
+     selectedVideo: res.data.items[0] 
+    })
  }
 
  onVideoSelect = video =>{
-   console.log(video)
+   this.setState ({ selectedVideo: video})
+  //  console.log(video)
  }
 
   render(){
     return(
       <div className = "ui container">
         <SearchBar onFormSubmit = {this.onTermSubmit} />
+        <div className = 'ui grid'>
+          <div className = 'ui row'>
+            <div className = 'eleven wide column'>
+        <VideoDetail video = {this.state.selectedVideo}/>
+            </div>
+            <div className = "five wide column">
         <VideoList onVideoSelect = {this.onVideoSelect} 
         videos = {this.state.videos} />
+            </div>
+        </div>
+        </div>
         {/* There are {this.state.videos.length} Videos found */}
+        
       </div>
     )
   }
--- src/components/VideoItems.js
+++ src/components/VideoItems.js
@@ -4,8 +4,8 @@ import "./VideoItem.css"
 
 const VideoItem = ({video, onVideoSelect}) => {
   return (
-  <div onclick = { () => onVideoSelect(video)} className = 'video-item item'>
-     <img className = "ui image" alt = " " 
+  <div onClick = { () => onVideoSelect(video)} className = 'video-item item'>
+     <img className = "ui image" alt = "video.snippet.title" 
      src = {video.snippet.thumbnails.medium.url}/>
      <div className = "content">
      <div className = "header"> {video.snippet.title} </div>
